e228c5b372f7b4874bc792a9d4a2b8beff7e9352
itarek99 <tarekulislam7060@gmail.com>
2022-11-08 19:08:34 +0600

    services get request added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -7,6 +8,35 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ybjhadr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const servicesCollection = client.db('pixelPhotography').collection('services');
+
+    app.get('/services', async (req, res) => {
+      const { size } = req.query;
+      if (!size) {
+        const cursor = servicesCollection.find({});
+        const services = await cursor.toArray();
+        res.send(services);
+      } else {
+        const cursor = servicesCollection.find({});
+        const services = await cursor.limit(+size).toArray();
+        res.send(services);
+      }
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.error(err));
 
 app.get('/', (req, res) => {
   res.send('gf server is running');
