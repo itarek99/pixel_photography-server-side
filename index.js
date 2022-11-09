9679300ff43a2d150deb21ef717423175e34394b
itarek99 <tarekulislam7060@gmail.com>
2022-11-09 21:16:15 +0600

    review delete request complete

--- index.js
+++ index.js
@@ -64,6 +64,13 @@ const run = async () => {
       const result = await reviewCollection.insertOne(review);
       console.log(result);
     });
+
+    app.delete('/orders/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
   }
 };
